ca7d794bafbf55855a2b9abb96fa261c93a21abc
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-10-04 15:26:52 +0300

    hw 4

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,20 @@ public class Main {
     public static void main(String[] args) {
         int clientOS = 0;
         int clientDeviceYear = 2009;
-        if (clientOS == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
 
-        } else if (clientOS == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
+
         int year = 2000;
-        if (year > 1584 && year % 4 == 0) {
+        if (year >= 1584 && year % 4 == 0 && year % 100 != 0) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
