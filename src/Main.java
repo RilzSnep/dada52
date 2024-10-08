11c4f8a0a3d968279a76fdb8d29a29fb0e8ae985
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-10-08 18:41:54 +0300

    hw array

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         int[] array1 = new int[]{1, 2, 3};
         double[] array2 = {1.57d, 7.654d, 9.986d};
         int[] array3 = {52, 52, 52};
+
+
         for (int i = 0; i <= array1.length - 1; i++) {
             if (i != array2.length - 1) {
                 System.out.print(array1[i] + ", ");
@@ -54,13 +56,12 @@ public class Main {
 
 
         for (int i = 0; i <= array1.length - 1; i++) {
-            if (array1[i] % 2 == 1) {
-                array1[i] = array1[i] + 1;
-
+            if (array1[i] % 2 != 0) {
+                array1[i] += 1;
             }
         }
+        //результат преобразования
         System.out.println(Arrays.toString(array1));
-
 
     }
 }
