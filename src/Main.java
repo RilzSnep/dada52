f67f189b1b25fb8fa297da8f0765784cf2e2c098
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-11-19 17:20:12 +0300

    oop home work

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        Author firstAuthor = new Author("Александ ", "Пушкин ");
-        Author secondAuthor = new Author("Лев ", "Толстой ");
-
-        Book firstBook = new Book("7 гномов", firstAuthor, 20);
-        Book secondBook = new Book("Красавица и чудовище", secondAuthor, 20);
-        System.out.println("Книга " + firstBook.getName() + ", написал " + firstBook.getAuthor().getName() + firstBook.getAuthor().getSurname() +" назад");
-        System.out.println("Книга " + secondBook.getName() + ", написал " + secondBook.getAuthor().getName() + secondBook.getAuthor().getSurname() + " назад");
-        System.out.println(" ");
-        System.out.println(firstBook.equals(secondBook));
-        System.out.println(firstAuthor);
-        System.out.println(secondAuthor);
-        System.out.println(firstAuthor.equals(secondAuthor));
-        System.out.println(firstBook.hashCode());
-        System.out.println(secondBook.hashCode());
-        System.out.println(firstBook.hashCode());
-        System.out.println(secondBook.hashCode());
+        griffendor hermoine = new griffendor("Гермиона Грейнджер", 70, 35, 7, 6, 6);
+        griffendor poter = new griffendor("Гарри Поттер", 85, 45, 8, 9, 5);
+        hogwarts.describeStudent(hermoine);
+        hogwarts.comparisonStudentsMagicPower(hermoine, poter);
+        griffendor.comparisonGriffendorStudentPower(hermoine, poter);
+        pufendui cedric = new pufendui("Седрик Диггори", 88, 55, 6, 7, 8);
+        hogwarts.comparisonStudentsMagicPower(cedric, poter);
     }
 }
